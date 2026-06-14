3b8912589d2f8211cc7e5a86d0f9c07d400bc950
Samuel-C <samuelcamilo8@gmail.com>
2026-06-13 23:31:57 -0300

    bomb card stuff

--- scripts/game/card/bomb_card.gd
+++ scripts/game/card/bomb_card.gd
@@ -1,30 +1,11 @@
 class_name UmCard
 extends Card
 
-enum CardColor { YELLOW, RED, GREEN, BLUE, WILD }
-enum CardValue {
-	ZERO = 0,
-	ONE = 1,
-	TWO = 2,
-	THREE = 3,
-	FOUR = 4,
-	FIVE = 5,
-	SIX = 6,
-	SEVEN = 7,
-	EIGHT = 8,
-	NINE = 9,
-	SKIP = 10,
-	REVERSE = 11,
-	PLUSTWO = 12,
-	PLUSFOUR = 13
-}
-
 var snap_pos: Vector2
 var snap_rot: float
 
 @onready var title_label: Label = $Panel/MarginContainer/Front/Title
-@onready var aux_label: Label = $Panel/MarginContainer/Front/Label
-@onready var color_type: ColorRect = $Panel/MarginContainer/Front/ColorRect
+@onready var card_image: TextureRect = $Panel/MarginContainer/Front/TextureRect
 @onready var back = $Panel/Back
 
 
@@ -39,12 +20,12 @@ func _ready() -> void:
 
 func _apply_visual() -> void:
 	if world and entity_id >= 0 and world.entities.exists(entity_id):
-		if not world.has_component(entity_id, UnoCardComponent):
+		if not world.has_component(entity_id, BombCardComponent):
 			return
 		var comp = world.get_component(entity_id, CardComponent) as CardComponent
-		var uno = world.get_component(entity_id, UnoCardComponent) as UnoCardComponent
-		if comp and uno:
-			_apply_from_component(comp, uno)
+		var bomb = world.get_component(entity_id, BombCardComponent) as BombCardComponent
+		if comp and bomb:
+			_apply_from_component(comp, bomb)
 			return
 
 
@@ -52,38 +33,11 @@ func update_visual() -> void:
 	_apply_visual()
 
 
-func _apply_from_component(comp: CardComponent, uno: UnoCardComponent) -> void:
+func _apply_from_component(comp: CardComponent, b_comp: BombCardComponent) -> void:
 	var show_front = comp.face_up or comp.zone_id == multiplayer.get_unique_id()
-	var color = uno.color as CardColor
 	flip(not show_front)
-	title_label.text = uno.card_name
-	aux_label.text = uno.card_name
-	_apply_color(color)
-
-
-func _apply_color(color: CardColor) -> void:
-	match color:
-		CardColor.YELLOW:
-			color_type.material = null
-			color_type.color = Color.YELLOW
-		CardColor.RED:
-			color_type.material = null
-			color_type.color = Color.FIREBRICK
-		CardColor.GREEN:
-			color_type.material = null
-			color_type.color = Color.SEA_GREEN
-		CardColor.BLUE:
-			color_type.material = null
-			color_type.color = Color.NAVY_BLUE
-		CardColor.WILD:
-			var shader = load("res://assets/card.gdshader")
-			var shader_mat = ShaderMaterial.new()
-			shader_mat.shader = shader
-			color_type.material = shader_mat
-
-
-func _value_name(value: CardValue) -> String:
-	return CardValue.keys()[value].capitalize()
+	title_label.text = b_comp.card_name
+	card_image.texture = b_comp.card_image
 
 
 func _on_gui_input(event: InputEvent) -> void:
---
+++ scripts/game/components/bomb_card_data.gd
@@ -0,0 +1,5 @@
+class_name BombCardData
+extends CardData
+
+@export var card_name: String = "Nova Carta"
+@export var card_image: Texture2D = preload("res://assets/cards/bomb.png")
